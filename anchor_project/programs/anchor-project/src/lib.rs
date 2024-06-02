19ecef84aa22028990b41f8705ff94d0ece3cf2a
Naty-S <na7a5cha010@gmail.com>
2024-06-02 19:19:33 -0400

    anchor

--- anchor_project/programs/anchor-project/src/lib.rs
+++ anchor_project/programs/anchor-project/src/lib.rs
@@ -1,15 +1,139 @@
 use anchor_lang::prelude::*;
 
+
 declare_id!("3kAVGR8RrGNnutMWiVYi2DVs12NuhKhtKGQpn1TPQy7V");
+
 
 #[program]
 pub mod anchor_project {
-    use super::*;
+  use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
-    }
+  pub fn get_key(ctx: Context<GetKey>, link: Pubkey) -> Result<()> {
+
+    let key = &mut ctx.accounts.lucky_key;
+  
+    key.hunter = ctx.accounts.hunter.key();
+    key.bump = ctx.bumps.lucky_key;
+    key.link = link;
+    key.used = false;
+    key.portal = 0;
+
+    Ok(())
+  }
+
+  pub fn use_key(ctx: Context<UseKey>, portal: u64) -> Result<()> {
+
+    let key = &mut ctx.accounts.lucky_key;
+    
+    key.used = true;
+    key.portal = portal;
+
+    Ok(())
+  }
+
+  pub fn claim_treasure(ctx: Context<ClaimTreasure>) -> Result<()> {
+
+    let key = &mut ctx.accounts.lucky_key;
+    let treasure = &mut ctx.accounts.treasure;
+  
+    require!(key.used, Errors::NoKey);
+    require!(key.portal > 0, Errors::NoPortal);
+    // require!(key.hunter == ctx.accounts.hunter.key(), Errors::)
+
+    treasure.hunter = ctx.accounts.hunter.key();
+    treasure.key = key.key();
+    treasure.bump = ctx.bumps.treasure;
+
+
+    Ok(())
+  }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(link: Pubkey)]
+pub struct GetKey<'info> {
+
+  pub system_program: Program<'info, System>,
+
+  #[account(mut)]
+  pub hunter: Signer<'info>,
+
+  #[account(
+    init,
+    payer = hunter,
+    space = LuckyKey::SIZE,
+    seeds = [link.key().as_ref(), "lucky_key".as_bytes(), hunter.key().as_ref()],
+    bump
+  )]
+  pub lucky_key: Account<'info, LuckyKey>,
+}
+
+#[derive(Accounts)]
+pub struct UseKey<'info> {
+
+  pub system_program: Program<'info, System>,
+
+  #[account(
+    mut,
+    seeds = [lucky_key.link.key().as_ref(), "lucky_key".as_bytes(), lucky_key.hunter.key().as_ref()],
+    bump = lucky_key.bump
+  )]
+  pub lucky_key: Account<'info, LuckyKey>,
+}
+
+#[derive(Accounts)]
+pub struct ClaimTreasure<'info> {
+
+  pub system_program: Program<'info, System>,
+
+  #[account(mut)]
+  pub hunter: Signer<'info>,
+
+  #[account(
+    init,
+    payer = hunter,
+    space = Treasure::SIZE,
+    seeds = ["treasure".as_bytes(), hunter.key().as_ref(), lucky_key.key().as_ref()],
+    bump
+  )]
+  pub treasure: Account<'info, Treasure>,
+
+  #[account(
+    mut,
+    seeds = [lucky_key.link.key().as_ref(), "lucky_key".as_bytes(), lucky_key.hunter.key().as_ref()],
+    bump = lucky_key.bump
+  )]
+  pub lucky_key: Account<'info, LuckyKey>,
+}
+
+#[account]
+pub struct LuckyKey {
+  pub link: Pubkey,
+  pub hunter: Pubkey,
+  pub used: bool,
+  pub portal: u64,
+  pub bump: u8
+}
+
+impl LuckyKey {
+  pub const SIZE: usize = 8 + 32 + 32 + 1 + 8 + 1;
+}
+
+#[account]
+pub struct Treasure {
+  pub hunter: Pubkey,
+  pub key: Pubkey,
+  pub bump: u8
+}
+
+impl Treasure {
+  pub const SIZE: usize = 8 + 32 + 32 + 1;
+}
+
+#[error_code]
+pub enum Errors {
+  #[msg("Cannot claim treasure without Key")]
+  NoKey,
+  #[msg("Cannot claim treasure without going trouhg a Portal")]
+  NoPortal,
+}
--- anchor_project/tests/anchor-project.ts
+++ anchor_project/tests/anchor-project.ts
@@ -1,16 +1,82 @@
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
+import * as web3 from "@solana/web3.js";
+import { BN } from "bn.js";
+
 import { AnchorProject } from "../target/types/anchor_project";
+
 
 describe("anchor-project", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.AnchorProject as Program<AnchorProject>;
+  const program = anchor.workspace.AnchorProject as anchor.Program<AnchorProject>;
+  const hunter1 = anchor.web3.Keypair.generate();
+  const hunter2 = anchor.web3.Keypair.generate();
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+
+  it("Start hunting with starter Key", async () => {
+
+    const startKey = anchor.web3.Keypair.generate();
+    const [key_pkey, key_bump] = getKeyAddress(startKey.publicKey, hunter1.publicKey, program.programId);
+
+    await airdrop(provider.connection, hunter1.publicKey);
+
+    await program.methods.getKey(key_pkey)
+      .accounts({
+        systemProgram: anchor.web3.SystemProgram.programId,
+        hunter: hunter1.publicKey,
+        luckyKey: key_pkey,
+      })
+      .signers([hunter1])
+      .rpc({ commitment: "confirmed" });
+
+    await checkKey(program, key_pkey, hunter1.publicKey, startKey.publicKey);
+  });
+
+  it("Hunter can use Key to open Portal", async () => {
+
+  });
+
+  it("Hunter can claim Treasure", async () => {
+
+  });
+
+  it("Hunter2 can't claim Hunter1's Treasure", async () => {
+
   });
 });
+
+
+class SolanaError {
+  static contains(logs, error): boolean {
+    const match = logs?.filter(s => s.includes(error));
+    return Boolean(match?.length)
+  }
+}
+
+
+async function airdrop(connection: any, address: any, amount = 1000000000) {
+  await connection.confirmTransaction(await connection.requestAirdrop(address, amount), "confirmed");
+}
+
+
+function getKeyAddress(link: web3.PublicKey, hunter: web3.PublicKey, programID: web3.PublicKey) {
+  return web3.PublicKey.findProgramAddressSync(
+    [
+      link.toBuffer(),
+      anchor.utils.bytes.utf8.encode("lucky_key"),
+      hunter.toBuffer()
+    ], programID);
+}
+
+async function checkKey(
+  program: anchor.Program<AnchorProject>,
+  key: web3.PublicKey,
+  hunter: web3.PublicKey,
+
+) {
+  let keyData = await program.account.luckyKey.fetch(key);
+
+  
+}
